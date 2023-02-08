f4107acac4c68b776c94f2b73829de58780da9e7
BreadOak <75024315+BreadOak@users.noreply.github.com>
2023-02-08 21:03:03 +0900

    Add files via upload

--- SSH_example.cpp
+++ SSH_example.cpp
@@ -1,21 +1,28 @@
 #include <libssh/libssh.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h> 
  
+int scp_read(ssh_session session);
+int scp_receive(ssh_session session, ssh_scp scp);
+
 int main()
 {
   ssh_session my_ssh_session;
   ssh_channel channel;
+  ssh_scp scp;
   int rc;
   int nbytes;
+  int size, mode;
+  char *filename, *buffer;
   char *password;
-  char buffer[256];
+  // char buffer[256];
   
   ///// Open session and set options /////
   my_ssh_session = ssh_new();
   if (my_ssh_session == NULL)
     exit(-1);
-  ssh_options_set(my_ssh_session, SSH_OPTIONS_HOST, "Your Address");
+  ssh_options_set(my_ssh_session, SSH_OPTIONS_HOST, "192.168.0.39");
   ssh_options_set(my_ssh_session, SSH_OPTIONS_USER, "user");
   
   // Connect to server
@@ -39,7 +46,7 @@ int main()
  
   ///// Authenticate ourselves /////
   // password = getpass("Password: ");
-  password = (char *)"Your Password";
+  password = (char *)"nrmk2013";
   rc = ssh_userauth_password(my_ssh_session, NULL, password);
   if (rc != SSH_AUTH_SUCCESS)
   {
@@ -50,50 +57,106 @@ int main()
     exit(-1);
   }
 
-  ///// Make Channel /////
-  channel = ssh_channel_new(my_ssh_session);
-  if (channel == NULL) return SSH_ERROR;
+  // ///// Make Channel /////
+  // channel = ssh_channel_new(my_ssh_session);
+  // if (channel == NULL) return SSH_ERROR;
  
-  rc = ssh_channel_open_session(channel);
-  if (rc != SSH_OK)
-  {
-    ssh_channel_free(channel);
-    return rc;
-  }
+  // rc = ssh_channel_open_session(channel);
+  // if (rc != SSH_OK)
+  // {
+  //   ssh_channel_free(channel);
+  //   return rc;
+  // }
 
-  ///// Request_exec /////
-  rc = ssh_channel_request_exec(channel, "ls -l");
-  if (rc != SSH_OK)
-  {
-    ssh_channel_close(channel);
-    ssh_channel_free(channel);
-    return rc;
-  }
+  // ///// Request_exec /////
+  // rc = ssh_channel_request_exec(channel, "cd release; sudo ./TCP_test_GUI 0 0 3 45");
+  // if (rc != SSH_OK)
+  // {
+  //   ssh_channel_close(channel);
+  //   ssh_channel_free(channel);
+  //   return rc;
+  // }
 
-  ///// Read from channel  /////
-  nbytes = ssh_channel_read(channel, buffer, sizeof(buffer), 0);
-  while (nbytes > 0)
+  // ssh_channel_send_eof(channel);
+  // ssh_channel_close(channel);
+  // ssh_channel_free(channel);
+
+  scp = ssh_scp_new
+    (my_ssh_session, SSH_SCP_READ, "release/CORE200_LoadTest_Data.csv");
+  if (scp == NULL)
   {
-    if (fwrite(buffer, 1, nbytes, stdout) != nbytes)
-    {
-      ssh_channel_close(channel);
-      ssh_channel_free(channel);
-      return SSH_ERROR;
-    }
-    nbytes = ssh_channel_read(channel, buffer, sizeof(buffer), 0);
-  }
-   
-  if (nbytes < 0)
-  {
-    ssh_channel_close(channel);
-    ssh_channel_free(channel);
+    fprintf(stderr, "Error allocating scp session: %s\n",
+            ssh_get_error(my_ssh_session));
     return SSH_ERROR;
   }
-  ssh_channel_send_eof(channel);
-  ssh_channel_close(channel);
-  ssh_channel_free(channel);
+ 
+  rc = ssh_scp_init(scp);
+  if (rc != SSH_OK)
+  {
+    fprintf(stderr, "Error initializing scp session: %s\n",
+            ssh_get_error(my_ssh_session));
+    ssh_scp_free(scp);
+    return rc;
+  }
+
+  rc = ssh_scp_pull_request(scp);
+  if (rc != SSH_SCP_REQUEST_NEWFILE)
+  {
+    fprintf(stderr, "Error receiving information about file: %s\n",
+            ssh_get_error(my_ssh_session));
+    return SSH_ERROR;
+  }
+ 
+  size = ssh_scp_request_get_size(scp);
+  filename = strdup(ssh_scp_request_get_filename(scp));
+  mode = ssh_scp_request_get_permissions(scp);
+  printf("Receiving file %s, size %d, permissions 0%o\n",
+          filename, size, mode);
+  free(filename);
+ 
+  buffer = (char *)malloc(size);
+  if (buffer == NULL)
+  {
+    fprintf(stderr, "Memory allocation error\n");
+    return SSH_ERROR;
+  }
+ 
+  ssh_scp_accept_request(scp);
+
+  int r = 0;
+
+  while (r < size) 
+  {
+      int st = ssh_scp_read(scp, buffer+r, size-r);
+      r += st;
+  }
+
+  printf("Done\n");
+
+  FILE*fp = fopen("CORE200_LoadTest_Data.csv","w");
+ 
+  // write(fp, buffer, size);
+  fwrite(buffer, size, 1, fp);
+  free(buffer);
+ 
+  rc = ssh_scp_pull_request(scp);
+  if (rc != SSH_SCP_REQUEST_EOF)
+  {
+    fprintf(stderr, "Unexpected request: %s\n",
+            ssh_get_error(my_ssh_session));
+    return SSH_ERROR;
+  }
+
+  fclose(fp);
+
+  ssh_scp_close(scp);
+  ssh_scp_free(scp);
 
   ssh_disconnect(my_ssh_session);
   ssh_free(my_ssh_session);
 }
+
+
+
+
 
